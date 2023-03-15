1be181bd1300e42c3a2f125723cff7b94848e6c0
sjh <zwielicht2@gmail.com>
2023-03-15 14:53:16 +0900

    :Init

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DefaulfLayout from '@/layouts/default/Index'
+import MainView from '@/modules/MainView'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'DefaultLayout',
+    component: DefaulfLayout,
+    children: [
+      {
+        path: '/',
+        name: 'MainView',
+        component: MainView
+      }
+    ]
+  }, {
+
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
