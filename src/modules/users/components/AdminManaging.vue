167ef764408aeb31681e46cdfac86716c8cedd62
sjh <zwielicht2@gmail.com>
2023-03-23 14:19:06 +0900

    :FIx 관리자 회원관리

--- src/modules/users/components/AdminManaging.vue
+++ src/modules/users/components/AdminManaging.vue
@@ -37,8 +37,6 @@
   검색 결과가 없습니다.
 </div>
 
-
-
       <div class="search-container">
   <label>
     검색 기준:
@@ -89,133 +87,136 @@
 </div>
     </div>
   </template>
-  
-  <script>
-import { ref, computed, onMounted } from 'vue';
-// user.js 파일에서 $getUser 함수를 가져옵니다.
-import { $getUser, $searchUsers, $deleteUsers, $updateUsermodal } from '@/api/user.js';
 
+<script>
+import { ref, computed, onMounted } from 'vue'
+// user.js 파일에서 $getUser 함수를 가져옵니다.
+import { $getUser, $searchUsers, $deleteUsers, $updateUsermodal } from '@/api/user.js'
+import { getJsonAxiosInstance } from '@/api'
+import { useUserStore } from '@/stores/users'
 
 export default {
   name: 'AdminManaging',
-  async created() {
-  try {
-    const response = await $getUser();
-    if (response.status === 200) {
-      memberList.value = response.data;
-      totalItems.value = response.data.length;
-    }
-  } catch (error) {
-    console.error('Error fetching member list:', error);
-  }
-},
-  setup() {
-    const currentPage = ref(1);
-    const itemsPerPage = ref(10);
-    const totalItems = ref(100); // 전체 회원 수를 설정하십시오. 실제 데이터에 따라 변경됩니다.
-    const memberList = ref([]); // 회원 목록 데이터입니다. 실제 데이터에 따라 변경됩니다.
-    const searchExecuted = ref(false);
+  setup () {
+    const currentPage = ref(1)
+    const itemsPerPage = ref(10)
+    const totalItems = ref(100) // 전체 회원 수를 설정하십시오. 실제 데이터에 따라 변경됩니다.
+    const memberList = ref([]) // 회원 목록 데이터입니다. 실제 데이터에 따라 변경됩니다.
+    const searchExecuted = ref(false)
     // 검색 기준 및 검색어를 저장하는 ref
-    const searchCriteria = ref('');
-    const searchKeyword = ref('');
+    const searchCriteria = ref('')
+    const searchKeyword = ref('')
 
-    const isModalOpen = ref(false);
-    const selectedUser = ref(null);
+    const isModalOpen = ref(false)
+    const selectedUser = ref(null)
 
-    const openModal = () => {
-  const checkedItems = memberList.value.filter(item => item.checked);
-  if (checkedItems.length !== 1) {
-    alert("수정할 회원을 하나만 선택해주세요.");
-    return;
-  }
-  selectedUser.value = { ...checkedItems[0] };
-  isModalOpen.value = true;
-};
+    const user = useUserStore()
+    const axios = getJsonAxiosInstance(user.getLoginUserInfo)
 
-const closeModal = () => {
-  isModalOpen.value = false;
-};
-
-const updateUsermodal = async () => {
-  try {
-    await $updateUsermodal(selectedUser.value);
-    alert("회원 정보가 수정되었습니다.");
-    closeModal();
-    await fetchUserList();
-  } catch (error) {
-    console.error("Error updating user:", error);
-  }
-};
-    
-    const searchSelected = async () => {
-  try {
-    const response = await $searchUsers({
-      searchCriteria: searchCriteria.value,
-      searchKeyword: searchKeyword.value
-    });
-
-    if (response.status === 200) {
-      memberList.value = response.data;
-      totalItems.value = response.data.length;
-      setCurrentPage(1);
-       }else {
-  memberList.value = [];
-  totalItems.value = 0;
-}
-searchExecuted.value = true;
-  } catch (error) {
-    console.error('Error searching users:', error);
-  }
-};
-
-// 선택 삭제 기능
-const deleteSelected = async () => {
-  const checkedItems = memberList.value.filter(item => item.checked);
-
-  if (checkedItems.length === 0) {
-    alert('삭제할 회원을 선택해주세요.');
-    return;
-  }
-
-  try {
-    await $deleteUsers(checkedItems.map(item => item.userNo));
-    alert('선택된 회원이 삭제되었습니다.');
-    await fetchUserList();
-  } catch (error) {
-    console.error('Error deleting users:', error);
-  }
-};
-
-  
-
-  const fetchUserList = async () => {
+    // 회원조회
+    async function getUsers () {
       try {
-        const response = await $getUser();
+        const response = await axios.get(`/api/admin/users?userNo=${user.getLoginUserInfo.userNo}`)
         if (response.status === 200) {
-          memberList.value = response.data;
-          totalItems.value = response.data.length;
+          memberList.value = response.data
+          totalItems.value = response.data.length
         }
       } catch (error) {
-        console.error('Error fetching user list:', error);
+        console.error('Error fetching member list:', error)
+      }
+    }
+
+    const openModal = () => {
+      const checkedItems = memberList.value.filter(item => item.checked)
+      if (checkedItems.length !== 1) {
+        alert('수정할 회원을 하나만 선택해주세요.')
+        return
+      }
+      selectedUser.value = { ...checkedItems[0] }
+      isModalOpen.value = true
+    }
+
+    const closeModal = () => {
+      isModalOpen.value = false
+    }
+
+    const updateUsermodal = async () => {
+      try {
+        await $updateUsermodal(selectedUser.value)
+        alert('회원 정보가 수정되었습니다.')
+        closeModal()
+        await fetchUserList()
+      } catch (error) {
+        console.error('Error updating user:', error)
+      }
+    }
+
+    const searchSelected = async () => {
+      try {
+        const response = await $searchUsers({
+          searchCriteria: searchCriteria.value,
+          searchKeyword: searchKeyword.value
+        })
+
+        if (response.status === 200) {
+          memberList.value = response.data
+          totalItems.value = response.data.length
+          setCurrentPage(1)
+        } else {
+          memberList.value = []
+          totalItems.value = 0
+        }
+        searchExecuted.value = true
+      } catch (error) {
+        console.error('Error searching users:', error)
+      }
+    }
+
+    // 선택 삭제 기능
+    const deleteSelected = async () => {
+      const checkedItems = memberList.value.filter(item => item.checked)
+
+      if (checkedItems.length === 0) {
+        alert('삭제할 회원을 선택해주세요.')
+        return
       }
 
-};
-    
+      try {
+        await $deleteUsers(checkedItems.map(item => item.userNo))
+        alert('선택된 회원이 삭제되었습니다.')
+        await fetchUserList()
+      } catch (error) {
+        console.error('Error deleting users:', error)
+      }
+    }
 
+    const fetchUserList = async () => {
+      try {
+        const response = await $getUser()
+        if (response.status === 200) {
+          memberList.value = response.data
+          totalItems.value = response.data.length
+        }
+      } catch (error) {
+        console.error('Error fetching user list:', error)
+      }
+    }
 
-    const totalPages = computed(() => Math.ceil(totalItems.value / itemsPerPage.value));
+    const totalPages = computed(() => Math.ceil(totalItems.value / itemsPerPage.value))
 
     const paginatedItems = computed(() => {
-      const start = (currentPage.value - 1) * itemsPerPage.value;
-      const end = start + itemsPerPage.value;
-      return memberList.value.slice(start, end);
-    });
+      const start = (currentPage.value - 1) * itemsPerPage.value
+      const end = start + itemsPerPage.value
+      return memberList.value.slice(start, end)
+    })
 
     const setCurrentPage = (page) => {
-      currentPage.value = page;
-    };
+      currentPage.value = page
+    }
 
-    onMounted(fetchUserList);
+    onMounted(() => {
+      getUsers()
+    })
 
     return {
       currentPage,
@@ -231,70 +232,70 @@ const deleteSelected = async () => {
       searchExecuted,
       deleteSelected,
       updateUsermodal,
-      openModal,      
-      closeModal,     
-      selectedUser,   
-      isModalOpen,    
-    };
-  },
-};
+      openModal,
+      closeModal,
+      selectedUser,
+      isModalOpen
+    }
+  }
+}
 </script>
-  
+
   <style scoped>
   .admin-container {
     font-family: 'Arial', sans-serif;
     padding: 20px;
   }
-  
+
   h1 {
     font-size: 32px;
     font-weight: bold;
     color: #333333;
     margin-bottom: 20px;
   }
-  
+
   .table-container {
     width: 100%;
     overflow-x: auto;
   }
-  
+
   table {
     width: 100%;
     border-collapse: collapse;
     margin-bottom: 20px;
   }
-  
+
   th,
   td {
     text-align: left;
     padding: 10px;
     border: 1px solid #cccccc;
   }
-  
+
   th {
     font-weight: bold;
     background-color: #f2f2f2;
   }
-  
+
   tbody tr:nth-child(even) {
     background-color: #f8f8f8;
   }
-  
+
   .search-container {
     display: flex;
     justify-content: space-between;
     margin-bottom: 20px;
   }
-  
+
   .search-container input {
     padding: 5px;
   }
-  
+
   .button-container {
     display: flex;
     justify-content: flex-end;
   }
-  
+
   button {
     padding: 10px 20px;
     font-size: 14px;
@@ -307,4 +308,3 @@ const deleteSelected = async () => {
     margin-left: 10px;
   }
   </style>
-  
